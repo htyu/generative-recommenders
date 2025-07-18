0ef573f4eb6b7772365325af7d537b6c32651d20
Hongtao Yu <hoy@fb.com>
2025-07-18 13:33:37 -0700

    _hstu_attn_fwd_compute_tlx

--- generative_recommenders/ops/triton/triton_hstu_attention.py
+++ generative_recommenders/ops/triton/triton_hstu_attention.py
@@ -63,150 +63,11 @@ def _get_fw_configs() -> List[triton.Config]:  # noqa: C901
     else:
         configs = [
             triton.Config(
-                {"BLOCK_M": 16, "BLOCK_N": 32},
-                num_stages=2,
-                num_warps=2,
-            ),
-            triton.Config(
-                {"BLOCK_M": 32, "BLOCK_N": 32},
-                num_stages=2,
-                num_warps=2,
-            ),
-            triton.Config(
-                {"BLOCK_M": 32, "BLOCK_N": 32},
-                num_stages=4,
-                num_warps=2,
-            ),
-            triton.Config(
-                {"BLOCK_M": 32, "BLOCK_N": 32},
-                num_stages=2,
-                num_warps=4,
-            ),
-            triton.Config(
-                {"BLOCK_M": 32, "BLOCK_N": 32},
-                num_stages=4,
-                num_warps=4,
-            ),
-            triton.Config(
-                {"BLOCK_M": 32, "BLOCK_N": 64},
-                num_stages=2,
-                num_warps=4,
-            ),
-            triton.Config(
-                {"BLOCK_M": 32, "BLOCK_N": 64},
-                num_stages=4,
-                num_warps=4,
-            ),
-            triton.Config(
-                {"BLOCK_M": 32, "BLOCK_N": 64},
+                {"BLOCK_M": 128, "BLOCK_N": 32, "USE_TLX": True,},
                 num_stages=4,
                 num_warps=8,
             ),
-            triton.Config(
-                {"BLOCK_M": 32, "BLOCK_N": 128},
-                num_stages=2,
-                num_warps=4,
-            ),
-            triton.Config(
-                {"BLOCK_M": 32, "BLOCK_N": 128},
-                num_stages=2,
-                num_warps=8,
-            ),
-            triton.Config(
-                {"BLOCK_M": 64, "BLOCK_N": 32},
-                num_stages=4,
-                num_warps=2,
-            ),
-            triton.Config(
-                {"BLOCK_M": 64, "BLOCK_N": 32},
-                num_stages=2,
-                num_warps=4,
-            ),
-            triton.Config(
-                {"BLOCK_M": 64, "BLOCK_N": 32},
-                num_stages=4,
-                num_warps=4,
-            ),
-            triton.Config(
-                {"BLOCK_M": 64, "BLOCK_N": 32},
-                num_stages=2,
-                num_warps=8,
-            ),
-            triton.Config(
-                {"BLOCK_M": 64, "BLOCK_N": 64},
-                num_stages=2,
-                num_warps=2,
-            ),
-            triton.Config(
-                {"BLOCK_M": 64, "BLOCK_N": 64},
-                num_stages=2,
-                num_warps=4,
-            ),
-            triton.Config(
-                {"BLOCK_M": 64, "BLOCK_N": 64},
-                num_stages=4,
-                num_warps=4,
-            ),
-            triton.Config(
-                {"BLOCK_M": 64, "BLOCK_N": 64},
-                num_stages=4,
-                num_warps=8,
-            ),
-            triton.Config(
-                {"BLOCK_M": 128, "BLOCK_N": 32},
-                num_stages=2,
-                num_warps=2,
-            ),
-            triton.Config(
-                {"BLOCK_M": 128, "BLOCK_N": 32},
-                num_stages=4,
-                num_warps=2,
-            ),
-            triton.Config(
-                {"BLOCK_M": 128, "BLOCK_N": 32},
-                num_stages=2,
-                num_warps=4,
-            ),
-            triton.Config(
-                {"BLOCK_M": 128, "BLOCK_N": 32},
-                num_stages=4,
-                num_warps=4,
-            ),
-            triton.Config(
-                {"BLOCK_M": 128, "BLOCK_N": 32},
-                num_stages=2,
-                num_warps=8,
-            ),
-            triton.Config(
-                {"BLOCK_M": 128, "BLOCK_N": 32},
-                num_stages=4,
-                num_warps=8,
-            ),
-            triton.Config(
-                {"BLOCK_M": 128, "BLOCK_N": 64},
-                num_stages=2,
-                num_warps=4,
-            ),
-            triton.Config(
-                {"BLOCK_M": 128, "BLOCK_N": 64},
-                num_stages=2,
-                num_warps=8,
-            ),
-            triton.Config(
-                {"BLOCK_M": 128, "BLOCK_N": 64},
-                num_stages=4,
-                num_warps=8,
-            ),
-            triton.Config(
-                {"BLOCK_M": 128, "BLOCK_N": 128},
-                num_stages=4,
-                num_warps=4,
-            ),
-            triton.Config(
-                {"BLOCK_M": 128, "BLOCK_N": 128},
-                num_stages=2,
-                num_warps=8,
-            ),
+
         ]
     return configs
 
@@ -610,6 +471,238 @@ def _hstu_attn_fwd_compute(  # noqa C901
                 )
 
 
+@triton.jit
+def _hstu_attn_fwd_compute_tlx(  # noqa C901
+    Q,
+    K,
+    V,
+    H,
+    DimQ,
+    DimV,
+    seq_offsets,
+    num_targets,
+    Out,
+    stride_qm,
+    stride_qh,
+    stride_kn,
+    stride_kh,
+    stride_vn,
+    stride_vh,
+    stride_om,
+    stride_oh,
+    alpha,
+    MAX_SEQ_LEN,
+    DeltaSize,
+    contextual_seq_len,
+    max_attn_len,
+    off_z,
+    off_h,
+    pid,
+    HAS_MULTIPLE_TARGETS: tl.constexpr,
+    IS_DELTA_Q: tl.constexpr,
+    ALLOW_TF32: tl.constexpr,
+    BLOCK_D_Q: tl.constexpr,
+    BLOCK_D_V: tl.constexpr,
+    BLOCK_M: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+    HAS_CONTEXTUAL_SEQ_LEN: tl.constexpr,
+    HAS_MAX_ATTN_LEN: tl.constexpr,
+):
+    seq_start = tl.load(seq_offsets + off_z).to(tl.int64)
+    off_h = off_h.to(tl.int64)
+    off_z = off_z.to(tl.int64)
+    seq_end = tl.load(seq_offsets + off_z + 1)
+    seq_len = (seq_end - seq_start).to(tl.int32)
+
+    if IS_DELTA_Q:
+        start_m_delta = pid * BLOCK_M
+        start_m = (start_m_delta + seq_len - DeltaSize).to(tl.int32)
+    else:
+        start_m_delta = 0
+        start_m = pid * BLOCK_M
+    if start_m < seq_len:
+        if HAS_MULTIPLE_TARGETS:
+            n_targets = tl.load(num_targets + off_z).to(tl.int32)
+        else:
+            n_targets = None
+
+        # initialize offsets
+        offs_m = start_m + tl.arange(0, BLOCK_M)
+        offs_n = tl.arange(0, BLOCK_N)
+        K_block_ptr = None
+        V_block_ptr = None
+        device_desc_k = tl.make_tensor_descriptor(
+            K,
+            shape=[seq_end.to(tl.int32), H * DimQ],
+            strides=[H * DimQ, 1],
+            block_shape=[BLOCK_N, BLOCK_D_Q],
+        )
+
+        device_desc_v = tl.make_tensor_descriptor(
+            V,
+            shape=[seq_end.to(tl.int32), H * DimV],
+            strides=[H * DimV, 1],
+            block_shape=[BLOCK_N, BLOCK_D_V],
+        )
+
+        if IS_DELTA_Q:
+            device_desc_q = tl.make_tensor_descriptor(
+                Q,
+                shape=[(off_z * DeltaSize + DeltaSize).to(tl.int32), H * DimQ],
+                strides=[H * DimQ, 1],
+                block_shape=[BLOCK_M, BLOCK_D_Q],
+            )
+
+            q = device_desc_q.load(
+                [
+                    (off_z * DeltaSize + start_m_delta).to(tl.int32),
+                    (off_h * stride_qh).to(tl.int32),
+                ]
+            )
+        else:
+            device_desc_q = tl.make_tensor_descriptor(
+                Q,
+                shape=[seq_end.to(tl.int32), H * DimQ],
+                strides=[H * DimQ, 1],
+                block_shape=[BLOCK_M, BLOCK_D_Q],
+            )
+            q = device_desc_q.load(
+                [
+                    (seq_start + start_m).to(tl.int32),
+                    (off_h * stride_qh).to(tl.int32),
+                ]
+            )
+
+        acc = tl.zeros([BLOCK_M, BLOCK_D_V], dtype=tl.float32)
+        if HAS_MULTIPLE_TARGETS:
+            uih_end = seq_len - n_targets
+        else:
+            uih_end = seq_len
+        if HAS_CONTEXTUAL_SEQ_LEN is True and start_m < contextual_seq_len:
+            # uih_end must be larger than start_m
+            low = 0
+            high = seq_len
+        else:
+            low = 0
+            high = start_m + BLOCK_M
+            if HAS_MAX_ATTN_LEN:
+                if start_m > uih_end:
+                    low = uih_end - max_attn_len
+                else:
+                    low = start_m - max_attn_len
+                if HAS_CONTEXTUAL_SEQ_LEN:
+                    low = low if low > contextual_seq_len else 0
+                else:
+                    low = low if low > 0 else 0
+            if HAS_MULTIPLE_TARGETS:
+                uih_end = (uih_end + BLOCK_N - 1) // BLOCK_N * BLOCK_N
+                if uih_end < start_m:
+                    high = seq_len - n_targets
+
+        end_n = low
+        for start_n in range(low, high, BLOCK_N):
+            acc += _hstu_attn_fwd_one_block(
+                start_n=start_n,
+                seq_len=seq_len,
+                offs_m=offs_m,
+                offs_n=offs_n + start_n,
+                q=q,
+                K=K,
+                V=V,
+                K_block_ptr=K_block_ptr,
+                V_block_ptr=V_block_ptr,
+                device_desc_k=device_desc_k,
+                device_desc_v=device_desc_v,
+                offset_kh=off_h * stride_kh,
+                offset_vh=off_h * stride_vh,
+                seq_start=seq_start,
+                n_targets=n_targets if HAS_MULTIPLE_TARGETS else None,
+                alpha=alpha,
+                MAX_SEQ_LEN=MAX_SEQ_LEN,
+                contextual_seq_len=contextual_seq_len,
+                max_attn_len=max_attn_len,
+                HAS_MULTIPLE_TARGETS=HAS_MULTIPLE_TARGETS,
+                HAS_CONTEXTUAL_SEQ_LEN=HAS_CONTEXTUAL_SEQ_LEN,
+                HAS_MAX_ATTN_LEN=HAS_MAX_ATTN_LEN,
+                ALLOW_TF32=ALLOW_TF32,
+                BLOCK_D_Q=BLOCK_D_Q,
+                BLOCK_D_V=BLOCK_D_V,
+                BLOCK_N=BLOCK_N,
+                ENABLE_TMA=True,
+            )
+            end_n += BLOCK_N
+
+        # pyre-ignore[61]
+        if uih_end < start_m:
+            low_delta = start_m
+            high_delta = start_m + BLOCK_M
+            offset = (low_delta - end_n).to(tl.int32)
+            for start_delta in tl.range(
+                low_delta, high_delta, BLOCK_N, num_stages=0
+            ):
+                acc += _hstu_attn_fwd_one_block(
+                    start_n=start_delta,
+                    seq_len=seq_len,
+                    offs_m=offs_m,
+                    offs_n=offs_n + start_delta,
+                    q=q,
+                    K=K,
+                    V=V,
+                    K_block_ptr=K_block_ptr,
+                    V_block_ptr=V_block_ptr,
+                    device_desc_k=device_desc_k,
+                    device_desc_v=device_desc_v,
+                    offset_kh=off_h * stride_kh,
+                    offset_vh=off_h * stride_vh,
+                    seq_start=seq_start,
+                    n_targets=n_targets if HAS_MULTIPLE_TARGETS else None,
+                    alpha=alpha,
+                    MAX_SEQ_LEN=MAX_SEQ_LEN,
+                    contextual_seq_len=contextual_seq_len,
+                    max_attn_len=max_attn_len,
+                    HAS_MULTIPLE_TARGETS=HAS_MULTIPLE_TARGETS,
+                    HAS_CONTEXTUAL_SEQ_LEN=HAS_CONTEXTUAL_SEQ_LEN,
+                    HAS_MAX_ATTN_LEN=HAS_MAX_ATTN_LEN,
+                    ALLOW_TF32=ALLOW_TF32,
+                    BLOCK_D_Q=BLOCK_D_Q,
+                    BLOCK_D_V=BLOCK_D_V,
+                    BLOCK_N=BLOCK_N,
+                    ENABLE_TMA=True,
+                )
+
+        # Important: must cast to proper dtype. If acc is float32, but
+        # TMA descriptor specifies float16, the program will run
+        # without crashes but produce wrong results.
+        acc = acc.to(Out.dtype.element_ty)
+        if IS_DELTA_Q:
+            device_desc_o = tl.make_tensor_descriptor(
+                Out,
+                shape=[(off_z * DeltaSize + DeltaSize).to(tl.int32), H * DimV],
+                strides=[H * DimV, 1],
+                block_shape=[BLOCK_M, BLOCK_D_V],
+            )
+            device_desc_o.store(
+                [
+                    (off_z * DeltaSize + pid * BLOCK_M).to(tl.int32),
+                    (off_h * stride_oh).to(tl.int32),
+                ],
+                acc,
+            )
+        else:
+            device_desc_o = tl.make_tensor_descriptor(
+                Out,
+                shape=[seq_end.to(tl.int32), H * DimV],
+                strides=[H * DimV, 1],
+                block_shape=[BLOCK_M, BLOCK_D_V],
+            )
+            device_desc_o.store(
+                [
+                    (seq_start + pid * BLOCK_M).to(tl.int32),
+                    (off_h * stride_oh).to(tl.int32),
+                ],
+                acc,
+            )
+
 @triton_autotune(
     configs=_get_fw_configs(),
     key=[
@@ -663,6 +756,7 @@ def _hstu_attn_fwd(  # noqa C901
     HAS_SORT_BY_LENGTH_INDICES: tl.constexpr,
     ENABLE_TMA: tl.constexpr,
     TMA_DESC_SIZE: tl.constexpr,
+    USE_TLX: tl.constexpr,
 ):
     off_hz = tl.program_id(1)
     off_z = off_hz // H
@@ -670,45 +764,84 @@ def _hstu_attn_fwd(  # noqa C901
         off_z = tl.load(sort_by_length_indices + off_z)
     off_h = off_hz % H
     pid = tl.program_id(0)
-    _hstu_attn_fwd_compute(
-        Q=Q,
-        K=K,
-        V=V,
-        H=H,
-        DimQ=DimQ,
-        DimV=DimV,
-        workspace_ptr=workspace_ptr,
-        seq_offsets=seq_offsets,
-        num_targets=num_targets,
-        Out=Out,
-        stride_qm=stride_qm,
-        stride_qh=stride_qh,
-        stride_kn=stride_kn,
-        stride_kh=stride_kh,
-        stride_vn=stride_vn,
-        stride_vh=stride_vh,
-        stride_om=stride_om,
-        stride_oh=stride_oh,
-        alpha=alpha,
-        MAX_SEQ_LEN=MAX_SEQ_LEN,
-        DeltaSize=DeltaSize,
-        contextual_seq_len=contextual_seq_len,
-        max_attn_len=max_attn_len,
-        off_z=off_z,
-        off_h=off_h,
-        pid=pid,
-        HAS_MULTIPLE_TARGETS=HAS_MULTIPLE_TARGETS,
-        IS_DELTA_Q=IS_DELTA_Q,
-        ALLOW_TF32=ALLOW_TF32,
-        BLOCK_D_Q=BLOCK_D_Q,
-        BLOCK_D_V=BLOCK_D_V,
-        HAS_CONTEXTUAL_SEQ_LEN=HAS_CONTEXTUAL_SEQ_LEN,
-        HAS_MAX_ATTN_LEN=HAS_MAX_ATTN_LEN,
-        BLOCK_M=BLOCK_M,
-        BLOCK_N=BLOCK_N,
-        ENABLE_TMA=ENABLE_TMA,
-        TMA_DESC_SIZE=TMA_DESC_SIZE,
-    )
+    if USE_TLX:
+        tl.static_assert(HAS_MULTIPLE_TARGETS is True)
+        _hstu_attn_fwd_compute_tlx(
+            Q=Q,
+            K=K,
+            V=V,
+            H=H,
+            DimQ=DimQ,
+            DimV=DimV,
+            seq_offsets=seq_offsets,
+            num_targets=num_targets,
+            Out=Out,
+            stride_qm=stride_qm,
+            stride_qh=stride_qh,
+            stride_kn=stride_kn,
+            stride_kh=stride_kh,
+            stride_vn=stride_vn,
+            stride_vh=stride_vh,
+            stride_om=stride_om,
+            stride_oh=stride_oh,
+            alpha=alpha,
+            MAX_SEQ_LEN=MAX_SEQ_LEN,
+            DeltaSize=DeltaSize,
+            contextual_seq_len=contextual_seq_len,
+            max_attn_len=max_attn_len,
+            off_z=off_z,
+            off_h=off_h,
+            pid=pid,
+            HAS_MULTIPLE_TARGETS=HAS_MULTIPLE_TARGETS,
+            IS_DELTA_Q=IS_DELTA_Q,
+            ALLOW_TF32=ALLOW_TF32,
+            BLOCK_D_Q=BLOCK_D_Q,
+            BLOCK_D_V=BLOCK_D_V,
+            HAS_CONTEXTUAL_SEQ_LEN=HAS_CONTEXTUAL_SEQ_LEN,
+            HAS_MAX_ATTN_LEN=HAS_MAX_ATTN_LEN,
+            BLOCK_M=BLOCK_M,
+            BLOCK_N=BLOCK_N,
+        )
+    else:
+        _hstu_attn_fwd_compute(
+            Q=Q,
+            K=K,
+            V=V,
+            H=H,
+            DimQ=DimQ,
+            DimV=DimV,
+            workspace_ptr=workspace_ptr,
+            seq_offsets=seq_offsets,
+            num_targets=num_targets,
+            Out=Out,
+            stride_qm=stride_qm,
+            stride_qh=stride_qh,
+            stride_kn=stride_kn,
+            stride_kh=stride_kh,
+            stride_vn=stride_vn,
+            stride_vh=stride_vh,
+            stride_om=stride_om,
+            stride_oh=stride_oh,
+            alpha=alpha,
+            MAX_SEQ_LEN=MAX_SEQ_LEN,
+            DeltaSize=DeltaSize,
+            contextual_seq_len=contextual_seq_len,
+            max_attn_len=max_attn_len,
+            off_z=off_z,
+            off_h=off_h,
+            pid=pid,
+            HAS_MULTIPLE_TARGETS=HAS_MULTIPLE_TARGETS,
+            IS_DELTA_Q=IS_DELTA_Q,
+            ALLOW_TF32=ALLOW_TF32,
+            BLOCK_D_Q=BLOCK_D_Q,
+            BLOCK_D_V=BLOCK_D_V,
+            HAS_CONTEXTUAL_SEQ_LEN=HAS_CONTEXTUAL_SEQ_LEN,
+            HAS_MAX_ATTN_LEN=HAS_MAX_ATTN_LEN,
+            BLOCK_M=BLOCK_M,
+            BLOCK_N=BLOCK_N,
+            ENABLE_TMA=ENABLE_TMA,
+            TMA_DESC_SIZE=TMA_DESC_SIZE,
+        )
 
 
 @triton_autotune(
